de8e8fe35eb1119c7ecc149d46c650da1bd73bf2
Milos Paunovic <paun992@hotmail.com>
2022-05-11 20:34:08 +0200

    Update composables.js

--- ui/src/composables.js
+++ ui/src/composables.js
@@ -3,6 +3,7 @@ import useFormChild from './composables/use-form-child.js'
 import useMeta from './composables/use-meta.js'
 import useQuasar from './composables/use-quasar.js'
 
+
 export {
   useDialogPluginComponent,
   useFormChild,
